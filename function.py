1516bd501b6f81b276aa5b4860f6754abfcba680
Subham-u <75112434+Subham-u@users.noreply.github.com>
2021-11-21 23:13:05 +0530

    Add files via upload
    function practise

---
+++ function.py
@@ -0,0 +1,26 @@
+items =list()
+items_1 = []
+type(items_1)
+dir(items)
+{
+    'name' : 'subham',
+    'age' : 17,
+    'place' : 'Banpur'
+}
+introduction = {
+    'name' : 'subham',
+    'age' : 17,
+    'place' : 'Banpur'
+}
+type(introduction)
+dir(introduction)
+introduction['name']
+introduction['place']
+introduction['age']
+introduction.get('address','India')
+introduction['course']='python zero to hero'
+introduction.update({'name':'Subham Mahapatra'})
+introduction.update({'name1':'subah' })
+introduction.keys()
+introduction.values()
+introduction.items()
